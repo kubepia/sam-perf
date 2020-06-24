c3d02026b999781c38b2331e23b738c9ec111d53
myguddy <seungil.han@gmail.com>
2020-06-24 20:56:04 +0900

    build test

---
+++ static-springboot/temp/src/test/java/net/kubepia/staticspringboot/StaticSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kubepia.staticspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaticSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
